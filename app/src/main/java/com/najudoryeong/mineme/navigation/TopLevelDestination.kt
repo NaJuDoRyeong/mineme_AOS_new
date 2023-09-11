6b3e3a74a041b7d08023ac5923781d19899d0fb9
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-11 19:12:12 +0900

    feat : bottomTap svg to ImageVector (#3)

--- app/src/main/java/com/najudoryeong/mineme/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/najudoryeong/mineme/navigation/TopLevelDestination.kt
@@ -1,6 +1,8 @@
 package com.najudoryeong.mineme.navigation
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.najudoryeong.mineme.core.designsystem.icon.DoIcons
 
 enum class TopLevelDestination(
     val selectedIcon: ImageVector,
@@ -10,9 +12,21 @@ enum class TopLevelDestination(
 ) {
 
     Home(
+        selectedIcon = DoIcons.HomeBorder,
+        unselectedIcon = DoIcons.Home,
+        iconTextId = forYouR.string.for_you,
+        titleTextId = R.string.app_name,
     ),
     Story(
+        selectedIcon = DoIcons.StoryBorder,
+        unselectedIcon = DoIcons.Story,
+        iconTextId = bookmarksR.string.saved,
+        titleTextId = bookmarksR.string.saved,
     ),
     Setting(
+        selectedIcon = DoIcons.SettingBorder,
+        unselectedIcon = DoIcons.Setting,
+        iconTextId = interestsR.string.interests,
+        titleTextId = interestsR.string.interests,
     )
 }
---
+++ core/designsystem/src/main/kotlin/com/najudoryeong/mineme/core/designsystem/icon/DoIcons.kt
@@ -0,0 +1,13 @@
+package com.najudoryeong.mineme.core.designsystem.icon
+
+
+import androidx.compose.material.icons.Icons
+
+object DoIcons {
+    val Home = Icons.Rounded.Home
+    val HomeBorder = Icons.Rounded.HomeBorder
+    val Story = Icons.Rounded.Story
+    val StoryBorder = Icons.Rounded.Storyborder
+    val Setting = Icons.Rounded.Setting
+    val SettingBorder = Icons.Rounded.SettingBorder
+}
