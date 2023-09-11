f1caafe4882cbcddf7aa5cc56ced0dc1c663d647
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-11 16:50:41 +0900

    build : core:datastore 초기 build 구현 (#1)

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("mineme.android.library")
+    id("mineme.android.library.jacoco")
+    id("mineme.android.hilt")
+    alias(libs.plugins.protobuf)
+}
+
+android {
+    defaultConfig {
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+    namespace = "com.najudoryeong.mineme.core.datastore"
+    testOptions {
+        unitTests {
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+
+// Setup protobuf configuration, generating lite Java and Kotlin classes
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+androidComponents.beforeVariants {
+    android.sourceSets.register(it.name) {
+        java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
+        kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
+    }
+}
