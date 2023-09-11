e27161823d59cb0ca19e1742ada06525fcc12ca5
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-11 17:14:56 +0900

    feat : uiState에 따른 splash 변경 구현 및 전체 화면 구현 (#1)

--- app/src/main/java/com/najudoryeong/mineme/MainActivity.kt
+++ app/src/main/java/com/najudoryeong/mineme/MainActivity.kt
@@ -2,16 +2,57 @@ package com.najudoryeong.mineme
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+
+    val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
 
         var uiState: MainActivityUiState by mutableStateOf(MainActivityUiState.Loading)
+
+
+        // ui state 수집
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }
+                    .collect()
+            }
+        }
+
+        // uiState에 따른 splash 구현
+        splashScreen.setKeepOnScreenCondition {
+            when (uiState) {
+                MainActivityUiState.Loading -> true
+                is MainActivityUiState.Success -> false
+            }
+        }
+
+
+        // 화면을 전체 화면 모드로 설정하므로 콘텐츠가 화면의 모든 가장자리까지 확장
+        // 이는 상태 표시줄 및 탐색 표시줄과 같은 시스템 UI가 콘텐츠 아래에 나타나도록
+
+        // 전체 화면 모드를 활성화
+        enableEdgeToEdge()
+
 
         super.onCreate(savedInstanceState)
     }
