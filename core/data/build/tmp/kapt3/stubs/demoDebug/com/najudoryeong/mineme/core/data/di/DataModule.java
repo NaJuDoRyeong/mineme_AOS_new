2c4db23b131ae392bee6dc0a94aacd215c41494f
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-15 16:04:37 +0900

    feat : NetworkMonitor bind 클래스 구현 (#3)

--- core/data/build/tmp/kapt3/stubs/demoDebug/com/najudoryeong/mineme/core/data/di/DataModule.java
+++ core/data/build/tmp/kapt3/stubs/demoDebug/com/najudoryeong/mineme/core/data/di/DataModule.java
@@ -1,7 +1,7 @@
 package com.najudoryeong.mineme.core.data.di;
 
 @dagger.Module
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\bg\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H\'\u00a8\u0006\u0006"}, d2 = {"Lcom/najudoryeong/mineme/core/data/di/DataModule;", "", "bindsUserDataRepository", "Lcom/najudoryeong/mineme/core/data/repository/UserDataRepository;", "userDataRepository", "Lcom/najudoryeong/mineme/core/data/repository/OfflineFirstUserDataRepository;", "data_demoDebug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\bg\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H\'J\u0010\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tH\'\u00a8\u0006\n"}, d2 = {"Lcom/najudoryeong/mineme/core/data/di/DataModule;", "", "bindsNetworkMonitor", "Lcom/najudoryeong/mineme/core/data/util/NetworkMonitor;", "networkMonitor", "Lcom/najudoryeong/mineme/core/data/util/ConnectivityManagerNetworkMonitor;", "bindsUserDataRepository", "Lcom/najudoryeong/mineme/core/data/repository/UserDataRepository;", "userDataRepository", "Lcom/najudoryeong/mineme/core/data/repository/OfflineFirstUserDataRepository;", "data_demoDebug"})
 @dagger.hilt.InstallIn(value = {dagger.hilt.components.SingletonComponent.class})
 public abstract interface DataModule {
     
@@ -9,4 +9,9 @@ public abstract interface DataModule {
     @org.jetbrains.annotations.NotNull
     public abstract com.najudoryeong.mineme.core.data.repository.UserDataRepository bindsUserDataRepository(@org.jetbrains.annotations.NotNull
     com.najudoryeong.mineme.core.data.repository.OfflineFirstUserDataRepository userDataRepository);
+    
+    @dagger.Binds
+    @org.jetbrains.annotations.NotNull
+    public abstract com.najudoryeong.mineme.core.data.util.NetworkMonitor bindsNetworkMonitor(@org.jetbrains.annotations.NotNull
+    com.najudoryeong.mineme.core.data.util.ConnectivityManagerNetworkMonitor networkMonitor);
 }
--- core/data/src/main/kotlin/com/najudoryeong/mineme/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/najudoryeong/mineme/core/data/di/DataModule.kt
@@ -2,6 +2,8 @@ package com.najudoryeong.mineme.core.data.di
 
 import com.najudoryeong.mineme.core.data.repository.OfflineFirstUserDataRepository
 import com.najudoryeong.mineme.core.data.repository.UserDataRepository
+import com.najudoryeong.mineme.core.data.util.ConnectivityManagerNetworkMonitor
+import com.najudoryeong.mineme.core.data.util.NetworkMonitor
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,6 +17,11 @@ interface DataModule {
     fun bindsUserDataRepository(
         userDataRepository: OfflineFirstUserDataRepository,
     ): UserDataRepository
+
+    @Binds
+    fun bindsNetworkMonitor(
+        networkMonitor: ConnectivityManagerNetworkMonitor,
+    ): NetworkMonitor
 
 
 }
