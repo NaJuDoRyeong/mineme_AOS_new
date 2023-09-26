fcb4cd9cec1801436f4f763cfa60f59039ea057e
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-26 21:22:54 +0900

    feat : custombottombutton component 구현 (#23)

---
+++ core/designsystem/src/main/kotlin/com/najudoryeong/mineme/core/designsystem/component/CustomBottomButton.kt
@@ -0,0 +1,38 @@
+package com.najudoryeong.mineme.core.designsystem.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun CustomBottomButton(
+    modifier: Modifier = Modifier,
+    @StringRes textRes: Int,
+    enabled: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFFDB94),
+            disabledContainerColor = Color.White
+        )
+    ) {
+        Text(
+            text = stringResource(id = textRes),
+            color = Color.Black,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+
+}
--- feature/story/src/main/kotlin/com/najudoryeong/mineme/feature/story/WriteStoryScreen.kt
+++ feature/story/src/main/kotlin/com/najudoryeong/mineme/feature/story/WriteStoryScreen.kt
@@ -1,41 +1,22 @@
 package com.najudoryeong.mineme.feature.story
 
-import android.app.DatePickerDialog
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsTopHeight
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
@@ -43,9 +24,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.najudoryeong.mineme.core.designsystem.component.AnimatedScreen
+import com.najudoryeong.mineme.core.designsystem.component.CustomBottomButton
 import com.najudoryeong.mineme.core.designsystem.component.DateDialog
-import com.najudoryeong.mineme.core.designsystem.component.Picker
-import com.najudoryeong.mineme.core.designsystem.component.rememberPickerState
 import com.najudoryeong.mineme.core.designsystem.icon.DoIcons
 import com.najudoryeong.mineme.core.ui.R.string
 import java.time.LocalDate
@@ -119,9 +99,11 @@ fun WriteStoryFirstPageScreen(
 
         Spacer(modifier = Modifier.weight(1f))
 
-        Button(onClick = onContinueClicked) {
-            Text("Continue")
-        }
+        CustomBottomButton(
+            textRes = R.string.choosePicture,
+            enabled = true,
+            onClick = onContinueClicked
+        )
     }
 }
 
@@ -141,7 +123,7 @@ fun WriteStorySecondPageScreen(
             onBackClick = onBackClick
         )
 
-        Spacer(modifier = Modifier.weight(1f)) // 이 Spacer가 나머지 공간을 모두 차지합니다.
+        Spacer(modifier = Modifier.weight(1f)) //나머지 공간
 
         Text(text = "second")
     }
@@ -160,7 +142,7 @@ fun WriteStoryToolBar(
     ) {
         Text(
             text = stringResource(id = R.string.writeStory),
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier.align(Alignment.Center),
         )
 
         IconButton(onClick = { onBackClick() }) {
