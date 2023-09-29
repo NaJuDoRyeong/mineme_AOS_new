abd5e849e12b45e18d5964459b0ef30a4c74bbca
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-29 19:31:01 +0900

    feat : SettingsItem 구현(#31)

--- feature/settings/build/tmp/kapt3/stubs/demoDebug/com/najudoryeong/mineme/feature/story/SettingScreenKt.java
+++ feature/settings/build/tmp/kapt3/stubs/demoDebug/com/najudoryeong/mineme/feature/story/SettingScreenKt.java
@@ -1,6 +1,6 @@
 package com.najudoryeong.mineme.feature.story;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a\u0012\u0010\u0000\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u0003H\u0001\u001a\u0012\u0010\u0004\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u0003H\u0001\u00a8\u0006\u0005"}, d2 = {"SettingsRoute", "", "modifier", "Landroidx/compose/ui/Modifier;", "SettingsScreen", "settings_demoDebug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000,\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\u001a3\u0010\u0000\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u0007H\u0007\u00f8\u0001\u0000\u00f8\u0001\u0001\u00a2\u0006\u0004\b\b\u0010\t\u001a4\u0010\n\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0003\u0010\u000b\u001a\u00020\f2\b\b\u0001\u0010\r\u001a\u00020\f2\f\u0010\u000e\u001a\b\u0012\u0004\u0012\u00020\u00010\u000fH\u0007\u001a\u0012\u0010\u0010\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u0003H\u0001\u001a\u0012\u0010\u0011\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u0003H\u0001\u0082\u0002\u000b\n\u0005\b\u00a1\u001e0\u0001\n\u0002\b\u0019\u00a8\u0006\u0012"}, d2 = {"Separator", "", "modifier", "Landroidx/compose/ui/Modifier;", "color", "Landroidx/compose/ui/graphics/Color;", "thickness", "Landroidx/compose/ui/unit/Dp;", "Separator-Hht5A8o", "(Landroidx/compose/ui/Modifier;JF)V", "SettingsItem", "iconRes", "", "descriptionRes", "navigateTo", "Lkotlin/Function0;", "SettingsRoute", "SettingsScreen", "settings_demoDebug"})
 public final class SettingScreenKt {
     
     @androidx.compose.runtime.Composable
@@ -11,5 +11,13 @@ public final class SettingScreenKt {
     @androidx.compose.runtime.Composable
     public static final void SettingsScreen(@org.jetbrains.annotations.NotNull
     androidx.compose.ui.Modifier modifier) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    public static final void SettingsItem(@org.jetbrains.annotations.NotNull
+    androidx.compose.ui.Modifier modifier, @androidx.annotation.DrawableRes
+    int iconRes, @androidx.annotation.StringRes
+    int descriptionRes, @org.jetbrains.annotations.NotNull
+    kotlin.jvm.functions.Function0<kotlin.Unit> navigateTo) {
     }
 }
--- feature/settings/src/main/kotlin/com/najudoryeong/mineme/feature/story/SettingScreen.kt
+++ feature/settings/src/main/kotlin/com/najudoryeong/mineme/feature/story/SettingScreen.kt
@@ -2,6 +2,7 @@ package com.najudoryeong.mineme.feature.story
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -17,9 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.najudoryeong.mineme.core.designsystem.component.DottedLine
 import com.najudoryeong.mineme.core.designsystem.icon.DoIcons
 import com.najudoryeong.mineme.feature.settings.R
 
@@ -30,28 +35,54 @@ internal fun SettingsRoute(
     SettingsScreen()
 }
 
+
 @Composable
 internal fun SettingsScreen(
     modifier: Modifier = Modifier
 ) {
 
 
-
     Column(modifier.fillMaxSize()) {
-        SettingsItem(iconRes = DoIcons.heart , description = "Your Setting Description")
+        SettingsItem(
+            iconRes = DoIcons.setting_default.resourceId,
+            descriptionRes = R.string.Account,
+            navigateTo = {}
+        )
+
+        SettingsItem(
+            iconRes = DoIcons.setting_notification.resourceId,
+            descriptionRes = R.string.notification,
+            navigateTo = {}
+        )
+
+        SettingsItem(
+            iconRes = DoIcons.setting_security.resourceId,
+            descriptionRes = R.string.security,
+            navigateTo = {}
+        )
+
+        SettingsItem(
+            iconRes = DoIcons.setting_default.resourceId,
+            descriptionRes = R.string.announcement,
+            navigateTo = {}
+        )
+
+        SettingsItem(
+            iconRes = DoIcons.setting_default.resourceId,
+            descriptionRes = R.string.contact,
+            navigateTo = {}
+        )
     }
 
 }
 
 
-
-
-
 @Composable
 fun SettingsItem(
-    @DrawableRes iconRes: Int,
+    modifier: Modifier = Modifier,
+    @DrawableRes iconRes: Int = DoIcons.setting_default.resourceId,
     @StringRes descriptionRes: Int,
-    modifier: Modifier = Modifier
+    navigateTo: () -> Unit
 ) {
     Row(
         modifier = modifier
@@ -69,3 +100,4 @@ fun SettingsItem(
         Text(text = stringResource(id = descriptionRes))
     }
 }
+
