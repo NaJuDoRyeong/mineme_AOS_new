950901b37453e64e715df461bd47050854e29af1
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-09-29 19:19:33 +0900

    feat : Settings Icon Image 추가(#31)

--- feature/settings/src/main/kotlin/com/najudoryeong/mineme/feature/story/SettingScreen.kt
+++ feature/settings/src/main/kotlin/com/najudoryeong/mineme/feature/story/SettingScreen.kt
@@ -1,10 +1,27 @@
 package com.najudoryeong.mineme.feature.story
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.najudoryeong.mineme.core.designsystem.icon.DoIcons
+import com.najudoryeong.mineme.feature.settings.R
 
 @Composable
 internal fun SettingsRoute(
@@ -17,7 +34,38 @@ internal fun SettingsRoute(
 internal fun SettingsScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier.fillMaxSize()) {
-        Text(text = "settings")
+
+
+
+    Column(modifier.fillMaxSize()) {
+        SettingsItem(iconRes = DoIcons.heart , description = "Your Setting Description")
+    }
+
+}
+
+
+
+
+
+@Composable
+fun SettingsItem(
+    @DrawableRes iconRes: Int,
+    @StringRes descriptionRes: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = stringResource(id = descriptionRes))
     }
 }
