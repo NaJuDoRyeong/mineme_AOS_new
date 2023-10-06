019f66d040463d4627afb37bb186dbe4b1a05bd6
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-10-06 18:22:59 +0900

    feat : startup benchmark 이미지 렌더링 까지 측정 구현 (#38)

--- benchmarks/src/main/java/com/najudoryeong/mineme/home/HomeActions.kt
+++ benchmarks/src/main/java/com/najudoryeong/mineme/home/HomeActions.kt
@@ -3,11 +3,11 @@ package com.najudoryeong.mineme.home
 import androidx.benchmark.macro.MacrobenchmarkScope
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Until
+import com.najudoryeong.mineme.waitAndFindObject
 
 fun MacrobenchmarkScope.homeYouWaitForContent() {
     // "loadingWheel"라는 리소스 ID를 가진 로딩 애니메이션(또는 UI 요소)이 사라질 때까지 최대 5초 동안 대기
     device.wait(Until.gone(By.res("loadingWheel")), 5_000)
 
-    // LoaddingWheel이 사라지더라도 완전히 로드가 되지않을 때 방지
-    //device.waitAndFindObject(By.res("home:Main"), 10_000)
+    device.waitAndFindObject(By.res("CoupleProfile:Image"), 10_000)
 }
--- benchmarks/src/main/java/com/najudoryeong/mineme/startup/StartupBenchmark.kt
+++ benchmarks/src/main/java/com/najudoryeong/mineme/startup/StartupBenchmark.kt
@@ -4,15 +4,14 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.benchmark.macro.BaselineProfileMode
 import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.najudoryeong.mineme.PACKAGE_NAME
 import com.najudoryeong.mineme.home.homeYouWaitForContent
-import com.najudoryeong.mineme.setTestJwtAndStartApp
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,18 +24,22 @@ class StartupBenchmark {
 
     @Test
     fun startupNoCompilation() = startup(CompilationMode.None())
-    /**
+
     @Test
     fun startupBaselineProfileDisabled() = startup(
-        CompilationMode.Partial(baselineProfileMode = BaselineProfileMode.Disable, warmupIterations = 1),
+        CompilationMode.Partial(
+            baselineProfileMode = BaselineProfileMode.Disable,
+            warmupIterations = 1
+        ),
     )
 
     @Test
-    fun startupBaselineProfile() = startup(CompilationMode.Partial(baselineProfileMode = BaselineProfileMode.Require))
+    fun startupBaselineProfile() =
+        startup(CompilationMode.Partial(baselineProfileMode = BaselineProfileMode.Require))
 
     @Test
     fun startupFullCompilation() = startup(CompilationMode.Full())
-**/
+
     private fun startup(compilationMode: CompilationMode) = benchmarkRule.measureRepeated(
         packageName = PACKAGE_NAME,
         metrics = listOf(StartupTimingMetric()),
